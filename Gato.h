cc302a49aa2422e197ca3abd15843e4e77049f7a
a01715561-art <a01715561@tec.mx>
2025-11-30 22:05:32 -0600

    Add files via upload

---
+++ Gato.h
@@ -0,0 +1,35 @@
+#ifndef GATO_H
+#define GATO_H
+
+#include <iostream>
+#include "IMascota.h"
+using namespace std;
+
+class Gato : public IMascota {
+private:
+    string nombre;
+    int edad;
+    string color;
+
+public:
+    Gato(const string& _nombre, int _edad, const string& _color)
+        : nombre(_nombre), edad(_edad), color(_color) {}
+
+    string getNombre() const override { return nombre; }
+    int getEdad() const override { return edad; }
+
+    void setNombre(const string& n) override { nombre = n; }
+    void setEdad(int e) override { edad = e; }
+
+    void mostrarInfo() const override {
+        cout << "Gato  -> Nombre: " << nombre
+             << ", Edad: " << edad
+             << ", Color: " << color << endl;
+    }
+
+    void maullar() const {
+        cout << nombre << " dice: ¡Miau!" << endl;
+    }
+};
+
+#endif
---
+++ Pez.h
@@ -0,0 +1,35 @@
+#ifndef PEZ_H
+#define PEZ_H
+
+#include <iostream>
+#include "IMascota.h"
+using namespace std;
+
+class Pez : public IMascota {
+private:
+    string nombre;
+    int edad;
+    float tamano;
+
+public:
+    Pez(const string& _nombre, int _edad, float _tamano)
+        : nombre(_nombre), edad(_edad), tamano(_tamano) {}
+
+    string getNombre() const override { return nombre; }
+    int getEdad() const override { return edad; }
+
+    void setNombre(const string& n) override { nombre = n; }
+    void setEdad(int e) override { edad = e; }
+
+    void mostrarInfo() const override {
+        cout << "Pez   -> Nombre: " << nombre
+             << ", Edad: " << edad
+             << ", Tamaño: " << tamano << " cm" << endl;
+    }
+
+    void nadar() const {
+        cout << nombre << " está nadando." << endl;
+    }
+};
+
+#endif
